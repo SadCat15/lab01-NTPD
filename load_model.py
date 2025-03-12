7b2671bf8cd76d02c664f61e0d980a4a54411daa
SadCat15 <michal.grzona777@gmail.com>
2025-03-12 15:24:32 +0100

    added model's version to model name

--- load_model.py
+++ load_model.py
@@ -2,7 +2,7 @@ import pickle
 
 import numpy as np
 
-with open('model.pkl', 'rb') as f:
+with open('model_1_0_0.pkl', 'rb') as f:
     model = pickle.load(f)
 
 x = np.array([5.1, 3.5, 1.4, 0.2]).reshape(1, -1)  # pierwszy element ze zbioru iris dataset from sklearn.dataset, # predykcja powinna zwrócić 0
